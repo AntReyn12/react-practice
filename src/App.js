b5c6491101e8dc73676ef15c207ba387690cbfed
ant_reyn <antreyn@udel.edu>
2021-07-19 12:53:05 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
-import TeamButton from "./ComboSkills";
 
 const App = () => {
-  const [showTeams, setShowTeams] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>My favorite teams</h1>
-        <button onClick={() => setShowTeams(!showTeams)}>
-          {showTeams ? "Hide favorite teams" : "Show favorite teams"}
-        </button>
-        {showTeams && <TeamButton />}
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 };
